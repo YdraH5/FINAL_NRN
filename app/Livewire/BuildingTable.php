f18ea6ec2408709ef24c3c15f31a19229ee1b4e8
unknown <hardy.aranzanso.5@gmail.com>
2025-06-01 00:03:06 +0800

    531-2

--- app/Livewire/BuildingTable.php
+++ app/Livewire/BuildingTable.php
@@ -70,7 +70,7 @@ class BuildingTable extends Component
     }
     public function render()
     {
-        return view('livewire.owner.apartment-table',[
+        return view('livewire.owner.building-table',[
             'buildings' => Building::all()]);
     }
 }
